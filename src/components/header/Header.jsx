fe2aa10764d5313d161cb1420acebce5ea53472d
bravemz <braveovie1960@gmail.com>
2023-05-25 23:27:55 +0100

    A new Changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className='container header_container'>
         <h5>Hi 👋, I'm</h5>
         <h1>Brave Ovie!</h1>
-        <h5 className="text-light">Fullstack .</h5>
+        <h5 className="text-light">Fullstack Developer.</h5>
         <CTA/>
         <HeaderSocial/>
            
